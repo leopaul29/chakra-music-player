31102a1c1ca942a8e5ff7f8575221a487d004b78
Leo-paul <leopaulmartin@hotmail.fr>
2021-01-25 14:19:44 +0100

    feature: add basic audio player

--- src/App.tsx
+++ src/App.tsx
@@ -18,19 +18,11 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+          <audio controls>
+            <script src=""></script>
+            <source src="musics/Pandrezz-Mithril.mp3" type="audio/mp3" />
+            Your browser does not support the audio tag.
+          </audio>
         </VStack>
       </Grid>
     </Box>
